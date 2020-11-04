66c4f1f9c0c48cfd5d29cf08870481275e18001c
Nicolas Nahornyj <NicolasNahornyj@users.noreply.github.com>
2020-11-04 09:24:20 +0100

    Update slave_receiver_DEE_VA.ino
    text is received and printed 15 char by line without proper word cut

--- slave_receiver_DEE_VA/slave_receiver_DEE_VA.ino
+++ slave_receiver_DEE_VA/slave_receiver_DEE_VA.ino
@@ -7,49 +7,69 @@ byte inByte;
 SoftwareSerial mySerial(RX_PIN, TX_PIN); // Declare SoftwareSerial obj first
 Adafruit_Thermal printer(&mySerial);
 boolean somethingToPrint = false;
+String recevedMessage = "Bonjour les copains je vais bien, et vous comment allez vous";
+String line1 = "";
+String line2 = "";
+String line3 = "";
+String line4 = "";
 
 void setup()
 {
   //printer
   pinMode(7, OUTPUT); digitalWrite(7, LOW);
   mySerial.begin(19200);  // Initialize SoftwareSerial
-  
-  
+  printer.begin();        // Init printer (same regardless of serial type)
+  printer.upsideDownOn();
+  printer.setSize('L'); 
   //wire
   Wire.begin(1);
   Serial.begin(19200);
   Wire.onReceive(receiveEvent);
-  //printing();
+ 
 }
 
 void loop()
 {
-  delay(100);
   if(somethingToPrint){
-    printing();
+    //printing();
   }
 }
 
 void receiveEvent()
 {
-  while (1 < Wire.available()) { // loop through all but the last
+  //recevedMessage="";
+  while (Wire.available()) { // loop through all but the last
     char c = Wire.read(); // receive byte as a character
-    Serial.print(c);         // print the character
+    //recevedMessage += c;
   }
-  int x = Wire.read();    // receive byte as an integer
-  Serial.println(x);         // print the integer
-  somethingToPrint = true;
+  
+  Serial.println(recevedMessage);         // print the integer
+  splitString();
+  
 }
 
 void printing(){
-  printer.begin();        // Init printer (same regardless of serial type)
-  printer.upsideDownOn();
-  printer.setSize('L'); 
-  printer.println(F("got something"));
+  printer.println("");
+  printer.println(line4);
+  printer.println(line3);
+  printer.println(line2);
+  printer.println(line1);
   printer.feed(2);
   printer.sleep();      // Tell printer to sleep
-  delay(200L);         // Sleep for 3 seconds
+  delay(3000L);         // Sleep for 3 seconds
   printer.wake();       // MUST wake() before printing again, even if reset
-  printer.setDefault(); // Restore printer to defaults
+  //printer.setDefault(); // Restore printer to defaults
   somethingToPrint = false;
+}
+
+void splitString(){
+	line1 = recevedMessage.substring(0,16);
+	line2 = recevedMessage.substring(16,31);
+	line3 = recevedMessage.substring(31,46);
+	line4 = recevedMessage.substring(46,61);
+	somethingToPrint = true;
+}
+
+void splitAndReturn(){
+	recevedMessage
 }
