7e9b5743327a91208bb164e404507fad7c82f04f
Nicolas Nahornyj <NicolasNahornyj@users.noreply.github.com>
2020-11-05 10:04:14 +0100

    Update secable_length.ino

--- secable_length/secable_length.ino
+++ secable_length/secable_length.ino
@@ -1,12 +1,15 @@
 
 
-String str ="Hello World! Here is some text. I need them to split into multiple lines without breaking the words. aaaaaaaaaaaaaaaaaaaaaaaaa";
+String str ="HelloWorld!Hereis some text. I need them to split into multiple lines without breaking the words.";
 const int wantedLength = 15;
 
-String lines [9];
-int c = 0;
-int cl = 0;
-int endOfLine = 0;
+String lines [10];
+int c = 0; // global cursor
+int cl = 0; // line cursor 
+int endOfLine = 0; // end line cursor
+int checklength = 0;
+int letterWithoutSpace = 0;
+
 
 void setup(){
   Serial.begin(115200);
@@ -17,32 +20,48 @@ void loop(){}
 
 void split(){
   c = 0;
-  cl =0;
-  endOfLine=0;
-int temp = str.indexOf(' ',c); //indexOf(val,from)
+  cl = 0;
+  endOfLine = 0;
 
   while(c<str.length()){
-   
-    if (str.charAt(c) == ' ' || c == str.length()-1)
+    
+    if (str.charAt(c) == ' ')
     {
+       letterWithoutSpace = 0;
       if(c>endOfLine+wantedLength){
-        c--; //if go too far from the end of actual line (cl)
-        while(str.charAt(c) != ' '){
+        c--; //if go too far from the end of actual line (cl)  
+        //checklength++;    
+        while(str.charAt(c) != ' '){          
           c--; //go back until we find a space
         }
         lines[cl]=str.substring(endOfLine,c); //take the selection and past it in the lines array
-        endOfLine = c+1; //+1 to ignore space at the begining of the line.
-        if(c!= str.length()-1){
-          cl++;
-        }
+        endOfLine = c+1; //+1 to ignore space at the begining of the line. 
+        cl++;
+        
+      }  
+    }else
+    {
+      letterWithoutSpace++;
+      if (letterWithoutSpace > wantedLength)
+      {
+        lines[cl]=str.substring(endOfLine,c); //take the selection and past it in the lines array
+        endOfLine = c; //+1 to ignore space at the begining of the line. 
+        cl++;
+        letterWithoutSpace = 0;
       }
     }
     c++;
+
   } 
+  while(str.charAt(c)!= ' '){
+          c--;
+  }
+  lines[cl]=str.substring(c+1,str.length());
+
 }
 
 void printResult(){
-  for (int i = 0; i < 9; i++)
+  for (int i = 0; i < 10; i++)
   {
       Serial.println(lines[i]);
   }
